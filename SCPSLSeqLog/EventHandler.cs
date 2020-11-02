7022f125cb14325cf3b8ccf406ffcacdd077d05b
Aidan <sir.meepington.1@gmail.com>
2020-11-02 21:52:18 +0000

    Add source.

---
+++ SCPSLSeqLog/EventHandler.cs
@@ -0,0 +1,39 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using SeqExiledLogs;
+using System;
+
+namespace SCPSLSeqLog
+{
+    public class EventHandler
+    {
+
+        private readonly Plugin _plugin;
+
+        public EventHandler(Plugin plugin) => _plugin = plugin;
+
+        public void OnJoined(JoinedEventArgs ev)
+        {
+            LogToSeq.Info("Player {Name} ({UserId}) joined the game from IP: {IP}", ev.Player.Nickname, ev.Player.UserId, ev.Player.IPAddress);
+        }
+
+        public void OnDied(DiedEventArgs ev)
+        {
+            Player target = ev.Target;
+            Player killer = ev.Killer;
+            string killedBy = ev.HitInformations.GetDamageType().name;
+            if (killer == null) {
+                LogToSeq.Info("Player {TargetName} ({TargetUserId}) died by {KilledBy}", target.Nickname, target.UserId);
+            } else
+            {
+                LogToSeq.Info("Player {TargetName} ({TargetUserId}) died to {KillerName} ({KillerUserId}) using {KilledBy}.",
+                    target.Nickname, target.UserId, killer.Nickname, killer.UserId, killedBy);
+            }
+        }
+
+        public void OnLeft(LeftEventArgs ev)
+        {
+            LogToSeq.Info("Player {Name} ({UserId}) left the game.", ev.Player.Nickname, ev.Player.UserId);
+        }
+    }
+}
---
+++ SCPSLSeqLog/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using Exiled.API.Features;
+
+namespace SCPSLSeqLog
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Author => "SirMeepington";
+
+        public override string Name => "Sample Seq Logging Plugin";
+
+
+        private EventHandler _events;
+
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+
+            _events = new EventHandler(this);
+
+            Exiled.Events.Handlers.Player.Joined += _events.OnJoined;
+            Exiled.Events.Handlers.Player.Left += _events.OnLeft;
+            Exiled.Events.Handlers.Player.Died += _events.OnDied;
+
+        }
+
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+
+            Exiled.Events.Handlers.Player.Joined -= _events.OnJoined;
+            Exiled.Events.Handlers.Player.Left -= _events.OnLeft;
+            Exiled.Events.Handlers.Player.Died -= _events.OnDied;
+
+
+            _events = null;
+        }
+
+    }
+}
